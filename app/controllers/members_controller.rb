b5c400cddb5ca996379df5d55dc38c24570f58cb
hiroki-ume <h.ume1219bknb@gmail.com>
2020-05-31 16:19:42 +0900

    chapter6 リソースの作成と更新

--- app/controllers/members_controller.rb
+++ app/controllers/members_controller.rb
@@ -8,23 +8,36 @@ class MembersController < ApplicationController
   end
 
   def new
-
+    @member = Member.new(birthday: Date.new(1995, 12, 19))
   end
 
   def create
-
+    @member = Member.new(params[:member])
+    if @member.save
+      redirect_to @member, notice: "会員を登録しました。"
+    else
+      render "new"
+    end
   end
 
   def edit
-
+    @member = Member.find(params[:id])
   end
 
   def update
-
+    @member = Member.find(params[:id])
+    @member.assign_attributes(params[:member])
+    if @member.save
+      redirect_to @member, notice: "会員情報を更新しました。"
+    else
+      render "edit"
+    end
   end
 
-  def dstroy
-
+  def destroy
+    @member = Member.find(params[:id])
+    @member.destroy
+    redirect_to :members, notice: "会員を削除しました。"
   end
 
   # 検索
