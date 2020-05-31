4f82ca6c8c50838038db00a5c11a19e5f26989ed
hiroki-ume <h.ume1219bknb@gmail.com>
2020-05-31 13:57:26 +0900

    membersコントローラーの作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   root "top#index"
   get 'about' => "top#about", as: "about"
+
+  resources :members do
+    get "search", on: :collection
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
