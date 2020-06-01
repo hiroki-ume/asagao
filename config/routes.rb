8c95a3373145435a17201d8842e6594ab0261cac
hiroki-ume <h.ume1219bknb@gmail.com>
2020-06-01 16:26:47 +0900

    chapter9 activerecordの活用

--- config/routes.rb
+++ config/routes.rb
@@ -12,5 +12,7 @@ Rails.application.routes.draw do
   resource :session, only: [:create, :destroy]
   resource :account, only: [:show, :edit, :update]
   resource :password, only: [:show, :edit, :update]
+
+  resources :articles
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
