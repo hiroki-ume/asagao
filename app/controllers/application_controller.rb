9e25e0aad1f56c2bba820d56dc03c11f9757ff4a
hiroki-ume <h.ume1219bknb@gmail.com>
2020-06-01 12:17:34 +0900

    deviseを使わないログイン認証の追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,17 @@
 class ApplicationController < ActionController::Base
+
+  class LoginRequired < StandardError; end
+  class Forbidden < StandardError; end
+
+  private
+
+  def login_required
+    raise LoginRequired unless current_member
+  end
+
+  def current_member
+    Member.find_by(id: session[:member_id]) if session[:member_id]
+  end
+  helper_method :current_member
+
 end
