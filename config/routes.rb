42e25e60ef5489107806e1af92d1ea0ed4312b79
hiroki-ume <h.ume1219bknb@gmail.com>
2020-06-06 11:43:06 +0900

    admin機能の追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
+  namespace :admin do
+    get 'top/index'
+  end
   root "top#index"
   get 'about' => "top#about", as: "about"
 
-  resources :members do
+  resources :members, only: [:index, :show] do
     get "search", on: :collection
     resources :entries, only: [:index]
   end
@@ -11,13 +14,21 @@ Rails.application.routes.draw do
   resource :account, only: [:show, :edit, :update]
   resource :password, only: [:show, :edit, :update]
 
-  resources :articles
+  resources :articles, only:[:index, :show]
   resources :entries do
     patch "like", "unlike", on: :member
     get "voted", on: :collection
     resources :images, controller: "entry_images" do
       patch :move_higher, :move_lower, on: :member
     end
+  end
+
+  namespace :admin do
+    root "top#index"
+    resources :members do
+      get "search", on: :collection
+    end
+    resources :articles
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
